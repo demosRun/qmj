563c6b11a1e5f9e6c0d33f6ac0e80288ced106b7
puge <mail@puge.org>
2021-04-02 17:45:57 +0800

    save

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ function scrollIntoView () {
 }
 
 // 阻止微信拖动
-document.body.addEventListener('touchmove', function (e) {
-  e.preventDefault() // 阻止默认的处理方式(阻止下拉滑动的效果)
-}, {passive: false})
+// document.body.addEventListener('touchmove', function (e) {
+//   e.preventDefault() // 阻止默认的处理方式(阻止下拉滑动的效果)
+// }, {passive: false})
 
 setTimeout(() => {
   autoScale({
